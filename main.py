5cc62e19a7ffbca6d06868c9b64e0143dae56dc2
EC2 Default User <ec2-user@ip-172-31-20-153.ap-northeast-1.compute.internal>
2022-06-22 02:18:14 +0000

    change main.py

--- main.py
+++ main.py
@@ -3,9 +3,21 @@ from pymongo import MongoClient
 from storeData import getDataFromBinance
 import os
 
+# make secret file
+try:
+    secretdir = 'secret'
+    os.mkdir(secretdir)
+except FileExistsError:
+    print('exist')
+        
+f = open('secret/binance_password.py','w')
+f.write("binance_api_key = '" + os.environ['X509'] +  "'")
+f.close()
+# secret end
+
 client = MongoClient(os.environ['mongouri'],
                      tls=True,
-                     tlsCertificateKeyFile=os.environ['X509'])
+                     tlsCertificateKeyFile='./secret/binance_password.py')
 
 def get_db_handle(db_name):
     db_handle = client[db_name]
